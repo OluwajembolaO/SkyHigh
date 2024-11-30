62b296ee967c5e165e8f6fff776efae6b8c9f0a2
eefward <edwardy347@gmail.com>
2024-11-30 16:12:19 -0500

    YES

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/chatbot')
+def chat():
+    return render_template('chat.html')
+
+@app.route('/video')
+def video():
+    return render_template('video.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
