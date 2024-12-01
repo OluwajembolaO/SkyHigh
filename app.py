0511c07ed37cb495623aab332c9ca0251f7ee13a
OluwajembolaO <jembola21@gmail.com>
2024-12-01 15:54:00 -0500

    SQLALCHEMY(will be changed to sqlite)

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 from flask import Flask, redirect, render_template, request, session
-
+from flask_sqlalchemy import SQLAlchemy
 from qotd import qotd
 from location import fetch_therapy_data
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
 q = qotd()
 
 @app.route("/")
 def home():
     q = qotd()
     return render_template("index.html", qu=q)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect('/')
+        else:
+            return render_template('login.html', error="Invalid email or password")
+    return render_template('login.html')
 
 @app.route("/video")
 def video():
