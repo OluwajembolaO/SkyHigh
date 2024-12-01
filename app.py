585818430b3a2553ccd10a66e246a18922e4f8db
GuestyTheBesty <guestythebesty841@gmail.com>
2024-12-01 16:54:31 -0500

    uh massive changes pls work

--- app.py
+++ app.py
@@ -1,50 +1,71 @@
+from flask import Flask, redirect, render_template, request, session, jsonify, session
+from flask_session import Session
+import sqlite3
 
-from flask import Flask, redirect, render_template, request, session, jsonify
-from qotd import qotd
-from location import fetch_therapy_data
+from functions import create_databases, fetch_therapy_data, generateImage, login_required, qotd
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 
-db = SQLAlchemy(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+con = sqlite3.connect("goals.db", check_same_thread=False)
+cur = con.cursor()
+
 
 q = qotd()
 
 @app.route("/")
+@login_required
 def home():
-    q = qotd()
     return render_template("index.html", qu=q)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username").rstrip()
+        password = request.form.get("password")
 
-        user = User.query.filter_by(email=email).first()
+        if not (username and password): return "Fill out all fields"
+        check = cur.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchall()
+        if not (check and check_password_hash(check[0][2], password)): return "Incorrect username or password"
 
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect('/')
-        else:
-            return render_template('login.html', error="Invalid email or password")
-    return render_template('login.html')
+        session["user_id"] = check[0][0]
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username").rstrip()
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmpassword")
+
+        if not (username and password and confirmation): return "Fill out all fields"
+        if password != confirmation: return "Password does not match retyped password"
+
+        try: cur.execute("INSERT INTO users (username, hash_password) VALUES (?, ?)", (username, generate_password_hash(password)))
+        except sqlite3.IntegrityError: return "Username taken"
+
+        con.commit()
+        return redirect("/login")
+    return render_template("signup.html")
 
 @app.route("/video")
+@login_required
 def video():
     return render_template("video.html")
 
 @app.route("/help")
+@login_required
 def help():
     return render_template("help.html")
 
 @app.route('/therapy', methods=['POST'])
+@login_required
 def find_therapy():
     data = request.get_json()
     if data['location'] == "" or 'location' not in data:
